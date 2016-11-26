a0c66693782d05b113ebffe82d23e1091b453c14
studiouscake <amanrags@gmail.com>
2016-11-26 18:40:58 +0530

    added a new page and new navs bars

---
+++ src/pages/newpage/newpage.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-newpage',
+  templateUrl: 'newpage.html'
+})
+
+export class NewPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
